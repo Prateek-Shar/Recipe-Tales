799fdbcc5e4451cd3406dd195c2295db183b2b06
Prateek-Shar <147049043+Prateek-Shar@users.noreply.github.com>
2026-06-24 00:29:21 +0530

    added skeleton to blog recipe page

--- app/blog_recipe/[blog_recipes]/page.tsx
+++ app/blog_recipe/[blog_recipes]/page.tsx
@@ -9,6 +9,7 @@ import steps from "@/public/Images/steps.png";
 import guide from "@/public/Images/guide.png"
 import blog from "@/public/Images/blog.png"
 import info from "@/public/Images/info.png"
+import { Skeleton } from "antd";
 
 interface Meal_det {
     Author_name : string,
@@ -22,6 +23,9 @@ interface Meal_det {
 }
 
 const blog_recipes = () => {
+
+    const [showSkeleton , setShowSkeleton] = useState<boolean>(true)
+    const [showStats , setShowStats] = useState<boolean>(false)
 
     const [mealDet , setMealDet] = useState<Meal_det[]>([])
     const search_para = useSearchParams()
@@ -41,7 +45,8 @@ const blog_recipes = () => {
 
         const data = await res.json()
         setMealDet(data.det)
-        console.log("Data Fetched Successfully")
+        setShowSkeleton(false)
+        setShowStats(true)
     }
 
     console.log(mealDet)
@@ -56,46 +61,55 @@ const blog_recipes = () => {
         <>
             <div className="w-full flex flex-col justify-center items-center">
                <p className="font-Capra text-4xl mt-15">{Meal_name}</p>
-
-                <div className="w-full flex justify-evenly items-start mt-20">
-                    <div className="w-[40%]">
-
-                    <div className="w-full flex">
-                        <Image src={ing} alt="ing" height={20} width={40} className="ml-2" />    
-                        <p className="font-Mogra text-3xl ml-5">Ingredients</p>
+                
+                {showSkeleton && (
+                    <div className="w-[80%] my-10">
+                        <Skeleton paragraph={{rows : 10}} active />
                     </div>
+                )}
 
-                    {mealDet.map((md, index) => (
-                        <div key={index}>
-                            {md.Ingredient.map((ingredient, idx) => (
-                                <div className="flex mt-5 nth-last-[1]:mb-5">
-                                <Image src={ut} alt="Ing Icon" height={20} width={50} className="p-3"/>
-                                <p key={idx} className="font-Poppins p-2 text-[20px] bg-[#fffbeb] w-[90%] rounded-2xl">{ingredient}</p>
-                                </div>
-                            ))}
+                {showStats && (
+                    <>
+                    <div className="w-full flex justify-evenly items-start mt-20">
+                        <div className="w-[40%]">
+
+                        <div className="w-full flex">
+                            <Image src={ing} alt="ing" height={20} width={40} className="ml-2" />    
+                            <p className="font-Mogra text-3xl ml-5">Ingredients</p>
                         </div>
-                    ))}
-                    </div>  
 
-                    <div className="w-[40%]">
+                        {mealDet.map((md, index) => (
+                            <div key={index}>
+                                {md.Ingredient.map((ingredient, idx) => (
+                                    <div className="flex mt-5 nth-last-[1]:mb-5">
+                                    <Image src={ut} alt="Ing Icon" height={20} width={50} className="p-3"/>
+                                    <p key={idx} className="font-Poppins p-2 text-[20px] bg-[#fffbeb] w-[90%] rounded-2xl">{ingredient}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                        </div>  
 
-                    <div className="w-full flex">
-                        <Image src={guide} alt="ing" height={20} width={40} className="ml-2" />    
-                        <p className="font-Mogra text-3xl ml-5">Instructions</p>
+                        <div className="w-[40%]">
+
+                        <div className="w-full flex">
+                            <Image src={guide} alt="ing" height={20} width={40} className="ml-2" />    
+                            <p className="font-Mogra text-3xl ml-5">Instructions</p>
+                        </div>
+
+                        {mealDet.map((md, index) => (
+                            <div key={index}>
+                                {md.Instructions.map((ins, idx) => (
+                                    <div className="flex mt-5 nth-last-[1]:mb-5 items-center">
+                                    <Image src={steps} alt="Ing Icon" height={20} width={50} className="p-3"/>
+                                    <p key={idx} className="font-Poppins p-2 text-[20px] bg-[#fffbeb] w-[90%] rounded-2xl">{ins}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                        </div>  
                     </div>
-
-                    {mealDet.map((md, index) => (
-                        <div key={index}>
-                            {md.Instructions.map((ins, idx) => (
-                                <div className="flex mt-5 nth-last-[1]:mb-5 items-center">
-                                <Image src={steps} alt="Ing Icon" height={20} width={50} className="p-3"/>
-                                <p key={idx} className="font-Poppins p-2 text-[20px] bg-[#fffbeb] w-[90%] rounded-2xl">{ins}</p>
-                                </div>
-                            ))}
-                        </div>
-                    ))}
-                    </div>  
-                </div>
+                
 
                 <div className="w-[85%] mt-20 bg-white rounded-2xl">
                     <div className="flex items-center p-2">
@@ -107,57 +121,66 @@ const blog_recipes = () => {
                         <p key={index} className="font-Poppins text-2xl p-2 mt-5">{md.Blog}</p>
                     ))}
                 </div>
+                </>
+                )}
 
-                <div className="w-[85%] bg-white my-20 rounded-2xl">
-                    <div className="flex items-center mt-3">
-                        <Image src={info} alt="Other details" height={25} width={40} className="p-2" />
-                        <p className="font-Mogra text-3xl pt-2">Other Details</p>
+                {showSkeleton && (
+                    <div className="w-[80%] my-10">
+                        <Skeleton paragraph={{rows : 8}} active />
                     </div>
+                )}
 
-                    <div className="flex mt-8">
-                        <div className="w-[20%] flex p-2">
-                            <p className="font-Poppins text-[20px] text-stone-800">Prep Time : </p>
-                            {mealDet.map((md , idx) => (
-                                <p className="font-Capra text-2xl pl-2"> {md.Prep_Time} mins</p>
-                            ))}
+
+                {showStats && (
+                    <div className="w-[85%] bg-white my-20 rounded-2xl">
+                        <div className="flex items-center mt-3">
+                            <Image src={info} alt="Other details" height={25} width={40} className="p-2" />
+                            <p className="font-Mogra text-3xl pt-2">Other Details</p>
                         </div>
 
-                        <div className="w-[20%] flex p-2">
-                            <p className="font-Poppins text-[20px] text-stone-800">Cook Time - </p>
-                            {mealDet.map((md , idx) => (
-                                <p className="font-Capra text-2xl pl-2">{md.Cook_Time} mins</p>
-                            ))}
+                        <div className="flex mt-8">
+                            <div className="w-[20%] flex p-2">
+                                <p className="font-Poppins text-[20px] text-stone-800">Prep Time : </p>
+                                {mealDet.map((md , idx) => (
+                                    <p className="font-Capra text-2xl pl-2"> {md.Prep_Time} mins</p>
+                                ))}
+                            </div>
+
+                            <div className="w-[20%] flex p-2">
+                                <p className="font-Poppins text-[20px] text-stone-800">Cook Time - </p>
+                                {mealDet.map((md , idx) => (
+                                    <p className="font-Capra text-2xl pl-2">{md.Cook_Time} mins</p>
+                                ))}
+                            </div>
+
+                            <div className="w-[20%] flex p-2">
+                                <p className="font-Poppins text-[20px] text-stone-800">Servings - </p>
+                                {mealDet.map((md , idx) => (
+                                    <p className="font-Capra text-2xl pl-2">{md.Servings}</p>
+                                ))}
+                            </div>
                         </div>
 
-                        <div className="w-[20%] flex p-2">
-                            <p className="font-Poppins text-[20px] text-stone-800">Servings - </p>
-                            {mealDet.map((md , idx) => (
-                                <p className="font-Capra text-2xl pl-2">{md.Servings}</p>
-                            ))}
+                        <div className="flex">
+                            <div className="w-[20%] flex p-2">
+                                <p className="font-Poppins text-[20px] text-stone-800">Tags - </p>
+                                {mealDet.map((md , idx) => (
+                                    <p className="font-Capra text-2xl pl-2">{md.Tags}</p>
+                                ))}
+                            </div>
+                        </div>
+
+                        <div className="flex">
+                            <div className="w-[20%] flex p-2">
+                                <p className="font-Poppins text-[20px] text-stone-800">Recipe Of - </p>
+                                {mealDet.map((md , idx) => (
+                                    <p className="font-Capra text-2xl pl-2">{md.Author_name}</p>
+                                ))}
+                            </div>
                         </div>
                     </div>
-
-                    <div className="flex">
-                        <div className="w-[20%] flex p-2">
-                            <p className="font-Poppins text-[20px] text-stone-800">Tags - </p>
-                            {mealDet.map((md , idx) => (
-                                <p className="font-Capra text-2xl pl-2">{md.Tags}</p>
-                            ))}
-                        </div>
-                    </div>
-
-                    <div className="flex">
-                        <div className="w-[20%] flex p-2">
-                            <p className="font-Poppins text-[20px] text-stone-800">Recipe Of - </p>
-                            {mealDet.map((md , idx) => (
-                                <p className="font-Capra text-2xl pl-2">{md.Author_name}</p>
-                            ))}
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-           
+                )}
+           </div>
 
         </>
     )
