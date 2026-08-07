0ba188c7c58db75eb91b69a9ed16713cdc9f2097
Prateek-Shar <147049043+Prateek-Shar@users.noreply.github.com>
2026-08-07 10:33:08 +0530

    added categories ui and redirection to diff recipes in responsive design

--- components/Catagory/catagoryCards.tsx
+++ components/Catagory/catagoryCards.tsx
@@ -13,7 +13,6 @@ import soup from "@/public/Images/soup.png"
 import momo from "@/public/Images/momo.png"
 import asian from "@/public/Images/noodle.png";
 import { useRef, useState } from "react";
-import axios from "axios";
 import { useRouter } from "next/navigation";
 
 
@@ -42,46 +41,149 @@ const Catagory_Cards = () => {
     return (
 
         <>
-        <div className="w-full xl:hidden mm:flex justify-evenly">
+        <div className="w-full xl:hidden mm:flex flex-col">
+            <div className="w-full flex justify-evenly">
+                <div className="xl:hidden mm:flex flex-col mm:w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer" onClick={() => { Search_category("Breakfast")} }>
+                    <div className="w-full xl:p-4 mm:p-1 flex justify-center xl:mt-0 mm:mt-2">
+                        <Image src={breakfast} alt="breakfast" className="object-contain w-[40%]"/>
+                    </div>
 
-            <div className="xl:hidden mm:flex flex-col mm:w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer" onClick={() => { Search_category("Breakfast")} }>
-                <div className="w-full xl:p-4 mm:p-1 flex justify-center xl:mt-0 mm:mt-2">
-                    <Image src={breakfast} alt="breakfast" className="object-contain w-[40%]"/>
+                    <div className="w-full flex justify-center xl:my-2 mm:mt-1 mm:mb-3">
+                        <p className="font-Poppins xl:text-[16px] mm:text-[10px]">Breakfast</p>
+                    </div>
                 </div>
 
-                <div className="w-full flex justify-center xl:my-2 mm:mt-1 mm:mb-3">
-                    <p className="font-Poppins xl:text-[16px] mm:text-[10px]">Breakfast</p>
+                <div className="xl:hidden mm:flex flex-col mm:w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer">
+                    <div className="w-full xl:p-4 mm:p-1 flex justify-center xl:mt-0 mm:mt-2">
+                        <Image src={lunch} alt="breakfast" className="object-contain w-[40%]"/>
+                    </div>
+
+                    <div className="w-full flex justify-center xl:my-2 mm:mt-1 mm:mb-3">
+                        <p className="font-Poppins xl:text-[16px] mm:text-[10px]">Lunch</p>
+                    </div>
+                </div>
+
+                <div className="xl:hidden mm:flex flex-col mm:w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer">
+                    <div className="w-full xl:p-4 xl:mt-0 mm:p-1 flex justify-center mm:mt-2">
+                        <Image src={dinner} alt="breakfast" className="object-contain w-[42%]"/>
+                    </div>
+
+                    <div className="w-full flex justify-center xl:my-2 mm:mt-1 mm:mb-3">
+                        <p className="font-Poppins mm:text-[10px]">Dinner</p>
+                    </div>
+                </div>
+
+                <div className="xl:hidden mm:flex flex-col mm:w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer">
+                    <div className="w-full xl:p-4 mm:p-0 flex justify-center xl:mt-0 mm:mt-2">
+                        <Image src={deserts} alt="breakfast" className="object-contain mm:w-[40%]"/>
+                    </div>
+
+                    <div className="w-full flex justify-center xl:mb-2 xl:my-0 mm:my-2">
+                        <p className="font-Poppins mm:text-[10px]">Deserts</p>
+                    </div>
                 </div>
             </div>
 
-            <div className="xl:hidden mm:flex flex-col mm:w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer">
-                <div className="w-full xl:p-4 mm:p-1 flex justify-center xl:mt-0 mm:mt-2">
-                    <Image src={lunch} alt="breakfast" className="object-contain w-[40%]"/>
-                </div>
+            <div className="w-full flex justify-end">
+                {seeAll && (
+                    <div className="xl:flex mm:flex pt-8 pointer-events-auto mr-5">
+                        <button className="group relative inline-flex items-center gap-3 px-6 py-2 bg-white text-black rounded-full font-bold tracking-wide overflow-hidden transition-transform hover:scale-105 active:scale-95" onClick={showAllCategories}>
+                            <span className="relative z-10 xl:text-[16px] mm:text-[13px]">See All</span>
+                            <Image src={right} alt="Redirect" className="w-4 h-4 relative z-10 group-hover:translate-x-1 transition-transform" />
+                            <div className="absolute inset-0 bg-blue-500 transform scale-x-0 group-hover:scale-x-100 transition-transform origin-left duration-300 ease-out opacity-10"></div>
+                        </button>
+                    </div>
+                )}
 
-                <div className="w-full flex justify-center xl:my-2 mm:mt-1 mm:mb-3">
-                    <p className="font-Poppins xl:text-[16px] mm:text-[10px]">Lunch</p>
-                </div>
-            </div>
+                {moreCategories && (
+                <div className="flex flex-col">
+                    <div className="w-full flex justify-evenly items-center">
+                        <div className="w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer" onClick={() => { Search_category("High Protein")} }>
+                            <div className="w-full xl:p-4 mm:p-0 flex justify-center xl:mt-0 mm:mt-2">
+                                <Image src={protein} alt="breakfast" className="object-contain mm:w-[35%]"/>
+                            </div>
 
-            <div className="xl:hidden mm:flex flex-col mm:w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer">
-                <div className="w-full xl:p-4 xl:mt-0 mm:p-1 flex justify-center mm:mt-2">
-                    <Image src={dinner} alt="breakfast" className="object-contain w-[42%]"/>
-                </div>
+                            <div className="w-full flex justify-center xl:mb-2 xl:my-0 mm:my-2">
+                                <p className="font-Poppins text-[10px]">High Protein</p>
+                            </div>
+                        </div>
 
-                <div className="w-full flex justify-center xl:my-2 mm:mt-1 mm:mb-3">
-                    <p className="font-Poppins mm:text-[10px]">Dinner</p>
-                </div>
-            </div>
+                        <div className="w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer" onClick={() => { Search_category("Quick Meals")} }>
+                            <div className="w-full xl:p-4 mm:p-0 flex justify-center xl:mt-0 mm:mt-2">
+                                <Image src={quick} alt="breakfast" className="object-contain mm:w-[40%]"/>
+                            </div>
 
-            <div className="xl:hidden mm:flex flex-col mm:w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer">
-                <div className="w-full xl:p-4 mm:p-0 flex justify-center xl:mt-0 mm:mt-2">
-                    <Image src={deserts} alt="breakfast" className="object-contain mm:w-[40%]"/>
-                </div>
+                            <div className="w-full flex justify-center xl:mb-2 xl:my-0 mm:my-2">
+                                <p className="font-Poppins text-[10px]">Quick Meals</p>
+                            </div>
+                        </div>
 
-                <div className="w-full flex justify-center xl:mb-2 xl:my-0 mm:my-2">
-                    <p className="font-Poppins mm:text-[10px]">Deserts</p>
+                        <div className="w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer" onClick={() => { Search_category("Italian")} }>
+                            <div className="w-full xl:p-4 mm:p-1 flex justify-center xl:mt-0 mm:mt-2">
+                                <Image src={pizza} alt="pizza" className="object-contain w-[40%]"/>
+                            </div>
+
+                            <div className="w-full flex justify-center xl:my-2 mm:mt-1 mm:mb-3">
+                                <p className="font-Poppins text-[10px]">Italian</p>
+                            </div>
+                        </div>
+
+                        <div className="w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer" onClick={() => { Search_category("Asian")} }>
+                            <div className="w-full xl:p-4 mm:p-1 flex justify-center xl:mt-0 mm:mt-2">
+                                <Image src={asian} alt="Noodle" className="object-contain w-[40%]"/>
+                            </div>
+
+                            <div className="w-full flex justify-center xl:my-2 mm:mt-1 mm:mb-3">
+                                <p className="font-Poppins text-[10px]">Asian</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="w-full flex justify-evenly items-center">
+                        <div className="w-[20%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer" onClick={() => { Search_category("South Indian")} }>
+                            <div className="w-full xl:p-4 xl:mt-0 mm:p-1 flex justify-center mm:mt-2">
+                                <Image src={dosa} alt="Dosa" className="object-contain w-[42%]"/>
+                            </div>
+
+                            <div className="w-full flex justify-center xl:my-2 mm:mt-1 mm:mb-3">
+                                <p className="font-Poppins text-[10px]">South Indian</p>
+                            </div>
+                        </div>
+
+                        <div className="w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer" onClick={() => { Search_category("Street Food")} }>
+                            <div className="w-full xl:p-4 mm:p-0 flex justify-center xl:mt-0 mm:mt-2">
+                                <Image src={momo} alt="Momo" className="object-contain mm:w-[40%]"/>
+                            </div>
+
+                            <div className="w-full flex justify-center xl:mb-2 xl:my-0 mm:my-2">
+                                <p className="font-Poppins text-[10px]">Street Food</p>
+                            </div>
+                        </div>
+
+                        <div className="w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer" onClick={() => { Search_category("Soups")} }>
+                            <div className="w-full xl:p-4 mm:p-0 flex justify-center xl:mt-0 mm:mt-2">
+                                <Image src={soup} alt="Soup" className="object-contain mm:w-[40%]"/>
+                            </div>
+
+                            <div className="w-full flex justify-center xl:mb-2 xl:my-0 mm:my-2">
+                                <p className="font-Poppins text-[10px]">Soups</p>
+                            </div>
+                        </div>
+
+                        <div className="w-[18%] bg-[#f2f1ff] border-2 border-[#eeeeee] rounded-4xl hover:cursor-pointer" onClick={() => { Search_category("Drinks")} }>
+                            <div className="w-full xl:p-4 mm:p-0 flex justify-center xl:mt-0 mm:mt-2">
+                                <Image src={drink} alt="Drinks" className="object-contain mm:w-[40%]"/>
+                            </div>
+
+                            <div className="w-full flex justify-center xl:mb-2 xl:my-0 mm:my-2">
+                                <p className="font-Poppins text-[10px]">Drinks</p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
+                )}
+
             </div>
 
         </div>
@@ -153,7 +255,7 @@ const Catagory_Cards = () => {
             <div className="w-full flex justify-end mt-10" ref={moreCategoriesDiv}>
 
                 {seeAll && (
-                <div className="pt-8 pointer-events-auto mr-5">
+                <div className="xl:flex mm:flex pt-8 pointer-events-auto mr-5">
                     <button className="group relative inline-flex items-center gap-3 px-6 py-2 bg-white text-black rounded-full font-bold tracking-wide overflow-hidden transition-transform hover:scale-105 active:scale-95" onClick={showAllCategories}>
                         <span className="relative z-10">See All</span>
                         <Image src={right} alt="Redirect" className="w-4 h-4 relative z-10 group-hover:translate-x-1 transition-transform" />
